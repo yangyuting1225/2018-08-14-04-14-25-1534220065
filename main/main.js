3b3f322bf93ad2e1f4bb06ac311f893b6f12f72f
yangyuting1225 <yangyuting1225@gmail.com>
2019-05-31 18:02:47 +0800

    submit

--- main/main.js
+++ main/main.js
@@ -1,4 +1,20 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(str) {
+   	const ChUP =    [`._.`, `...`, `._.`, `._.`, `...`, `._.`, `._.`, `._.`, `._.`, `._.`]
+  	const ChMid =   [`|.|`, `..|`, `._|`, `._|`, `|_|`, `|_.`, `|_.`, `..|`, `|_|`, `|_|`]
+  	const ChDown =  [`|_|`, `..|`, `|_.`, `._|`, `..|`, `._|`, `|_|`, `..|`, `|_|`, `..|`]
+  	var resultA = ""
+  	var resultB = ""
+  	var resultC = ""
+  	for (var i = 0; i < str.length; i++) {
+  		resultA += ChUP[str[i]]
+  		resultB += ChMid[str[i]]
+  		resultC += ChDown[str[i]]
+  		if(i != str.length-1){
+  			resultA += " "
+  			resultB += " "
+  			resultC += " "
+  		}
+  	}
+  	return (resultA + '\n' + resultB + '\n' + resultC + '\n')
 };
+
